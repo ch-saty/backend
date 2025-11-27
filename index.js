f516bf9356c8137b1312f481b392b7c9e3c05cea
Satyam <arpita@Satyams-MacBook-Pro.local>
2025-11-27 11:44:20 +0530

    token

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
--- socket.js
+++ socket.js
@@ -1,37 +1,40 @@
-import http from "http"
-import express from "express"
-import { Server } from "socket.io"
-const app=express()
-const server=http.createServer(app)
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
 
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        methods:["GET","POST"]
-    }
-})
+const app = express();
 
-const userSocketMap={}
+// FIX: ADD THIS
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-export const getSocketId=(receiverId)=>{
-return userSocketMap[receiverId]
-}
+const server = http.createServer(app);
 
-io.on("connection",(socket)=>{
-   const userId=socket.handshake.query.userId
-   if(userId!=undefined){
-    userSocketMap[userId]=socket.id
-   }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
- io.emit('getOnlineUsers',Object.keys(userSocketMap))  
+const userSocketMap = {};
 
+export const getSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
 
-socket.on('disconnect',()=>{
-    delete userSocketMap[userId]
-     io.emit('getOnlineUsers',Object.keys(userSocketMap))  
-})
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  if (userId != undefined) {
+    userSocketMap[userId] = socket.id;
+  }
 
-})
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-export {app,io, server}
+export { app, io, server };
