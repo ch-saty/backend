0b6b49755bdc2a2cbf5d3b3035d1719db4f30427
Satyam <arpita@Satyams-MacBook-Pro.local>
2025-11-27 22:56:17 +0530

    email push

--- config/Mail.js
+++ config/Mail.js
@@ -1,23 +1,24 @@
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
-dotenv.config()
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  // service: "Gmail",
+  host: "smtp.gmail.com",
   port: 465,
-  secure: true, // true for 465, false for other ports
+  secure: true,
   auth: {
-    user:process.env.EMAIL,
-    pass:process.env.EMAIL_PASS,
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
-const sendMail=async (to,otp)=>{
-await transporter.sendMail({
-    from:`${process.env.EMAIL}`,
+const sendMail = async (to, otp) => {
+  await transporter.sendMail({
+    from: `${process.env.EMAIL}`,
     to,
     subject: "Reset Your Password",
-    html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
-})
-}
+    html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`,
+  });
+};
 
-export default sendMail
+export default sendMail;
